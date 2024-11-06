dd02bf9068813c27ca9a58bf4e6f6ed071b3d7b1
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-06 18:25:46 -0500

    feat: add hit function will increase the hit property by 1 every time it is called on an object

--- src/classes/ship.js
+++ src/classes/ship.js
@@ -1,7 +1,11 @@
-class Ship {
+export default class Ship {
   constructor(length) {
     this.length = length;
     this.hits = 0;
     this.sunk = false;
+  }
+
+  hit() {
+    return (this.hits += 1);
   }
 }
