b9a87e66e22c514674393e9240631bfc7f57e958
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-07 17:27:00 -0500

    feat: add function to place ships on gameboard

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -9,10 +9,22 @@ export default class Gameboard {
     for (let i = 0; i < 10; i++) {
       const row = [];
       for (let j = 0; j < 10; j++) {
-        row.push([i, j]);
+        row.push(null);
       }
       board.push(row);
     }
     return board;
+  }
+
+  placeShip(ship, startingX, startingY, direction) {
+    const board = this.board;
+
+    for (let i = 0; i < ship.length; i++) {
+      if (direction === "horizontal") {
+        board[startingX][startingY + i] = ship;
+      } else if (direction === "vertical") {
+        board[startingX + i][startingY] = ship;
+      }
+    }
   }
 }
