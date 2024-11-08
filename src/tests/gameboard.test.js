0826d27c41f981f8daab06c04f93a396821da7db
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-08 13:59:30 -0500

    test: add tests for recieveAttack and isFleetSunk

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -36,4 +36,59 @@ describe("Gameboard", () => {
       testBoard.placeShip(ship, 0, 0, "horizontal");
     }).toThrow("Ship placement overlaps with another ship");
   });
+
+  test("Invalid attack location", () => {
+    expect(() => {
+      testBoard.recieveAttack(-1, 8);
+    }).toThrow("Invalid Coordinates to Attack!");
+  });
+
+  test("Coordinates Already Attacked, (hit)", () => {
+    testBoard.placeShip(ship, 0, 0, "vertical");
+    testBoard.recieveAttack(4, 0);
+
+    expect(() => {
+      testBoard.recieveAttack(4, 0);
+    }).toThrow("Coordinates have already been attacked!");
+  });
+
+  test("Coordinates Already Attacked, (miss)", () => {
+    testBoard.recieveAttack(7, 5);
+
+    expect(() => {
+      testBoard.recieveAttack(7, 5);
+    }).toThrow("Coordinates have already been attacked!");
+  });
+
+  test("Attack Hits Ship", () => {
+    testBoard.placeShip(ship, 0, 0, "vertical");
+    testBoard.recieveAttack(2, 0);
+
+    expect(testBoard.board[2][0]).toBe("hit");
+  });
+
+  test("Attack Misses Ship", () => {
+    testBoard.placeShip(ship, 0, 0, "vertical");
+    testBoard.recieveAttack(4, 7);
+
+    expect(testBoard.board[4][7]).toBe("miss");
+  });
+
+  test("Hit increases Ship hits property", () => {
+    testBoard.placeShip(ship, 0, 0, "vertical");
+    testBoard.recieveAttack(2, 0);
+    testBoard.recieveAttack(3, 0);
+
+    expect(testBoard.board[0][0].hits).toBe(2);
+  });
+
+  test("Fleet is sunk", () => {
+    expect(testBoard.isFleetSunk()).toBe(true);
+  });
+
+  test("Fleet is NOT sunk", () => {
+    testBoard.placeShip(ship, 0, 0, "vertical");
+
+    expect(testBoard.isFleetSunk()).toBe(false);
+  });
 });
