3cebbe41741c4c23214c1e8c2cd02c4d0a6e54b5
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-12 15:22:20 -0500

    feat: refactor renderBoards to add the cell index as an atrribute to each cell element

--- src/modules/domController.js
+++ src/modules/domController.js
@@ -3,30 +3,38 @@ function renderBoards(game) {
   const cpuContainer = document.querySelector(".player-two-container");
 
   const humanBoard = game.humanPlayer.board.board;
-
-  humanBoard.forEach((row) => {
-    row.forEach((cell) => {
-      const gridCellHuman = document.createElement("div");
-      gridCellHuman.classList.add("cell");
-      if (cell !== "hit" && cell !== "miss" && cell !== null) {
-        gridCellHuman.classList.add("ship");
-      }
-      humanContainer.appendChild(gridCellHuman);
-    });
-  });
-
   const cpuBoard = game.cpuPlayer.board.board;
 
-  cpuBoard.forEach((row) => {
-    row.forEach((cell) => {
+  for (let i = 0; i < humanBoard.length; i++) {
+    for (let j = 0; j < humanBoard[0].length; j++) {
+      const gridCellHuman = document.createElement("div");
+      gridCellHuman.setAttribute("data-coordinates", `${i}, ${j}`);
+      gridCellHuman.classList.add("cell");
+
       const gridCellCpu = document.createElement("div");
+      gridCellCpu.setAttribute("data-coordinates", `${i}, ${j}`);
       gridCellCpu.classList.add("cell");
-      if (cell !== "hit" && cell !== "miss" && cell !== null) {
+
+      if (
+        humanBoard[i][j] !== "hit" &&
+        humanBoard[i][j] !== "miss" &&
+        humanBoard[i][j] !== null
+      ) {
+        gridCellHuman.classList.add("ship");
+      }
+
+      if (
+        cpuBoard[i][j] !== "hit" &&
+        cpuBoard[i][j] !== "miss" &&
+        cpuBoard[i][j] !== null
+      ) {
         gridCellCpu.classList.add("ship");
       }
+
+      humanContainer.appendChild(gridCellHuman);
       cpuContainer.appendChild(gridCellCpu);
-    });
-  });
+    }
+  }
 }
 
 export { renderBoards };
