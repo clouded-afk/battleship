6506cc2dab1722b25d7baf5bdf5d60d9c950d550
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-11 19:11:06 -0500

    feat: add function to create players and their gameboards

--- src/modules/gameController.js
+++ src/modules/gameController.js
@@ -1,3 +1,21 @@
 import Gameboard from "../classes/gameboard";
 import Ship from "../classes/ship";
 import Player from "../classes/player";
+
+export default class Game {
+  constructor() {
+    this.humanPlayer = null;
+    this.cpuPlayer = null;
+  }
+
+  createPlayers() {
+    this.humanPlayer = new Player();
+    this.cpuPlayer = new Player();
+
+    this.humanPlayer.board = new Gameboard();
+    this.cpuPlayer.board = new Gameboard();
+
+    this.humanPlayer.board.createGameboard();
+    this.cpuPlayer.board.createGameboard();
+  }
+}
