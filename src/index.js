afcf484d9f0c8bf329b13f354a602ee11c583966
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-12 14:55:56 -0500

    test: add placeholder ships to gameboards

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import "./style.css";
 import Game from "./modules/gameController";
-import { renderBoard } from "./modules/domController";
+import Ship from "./classes/ship";
+import { renderBoards } from "./modules/domController";
 
 const game = new Game();
 game.createPlayers();
-renderBoard(game);
+
+const one = new Ship(5);
+const two = new Ship(4);
+const three = new Ship(3);
+const four = new Ship(3);
+const five = new Ship(2);
+
+game.humanPlayer.board.placeShip(one, 0, 0, "horizontal");
+game.humanPlayer.board.placeShip(two, 4, 5, "horizontal");
+game.humanPlayer.board.placeShip(three, 8, 4, "horizontal");
+game.humanPlayer.board.placeShip(four, 1, 0, "vertical");
+game.humanPlayer.board.placeShip(five, 5, 4, "vertical");
+
+game.cpuPlayer.board.placeShip(one, 0, 0, "horizontal");
+game.cpuPlayer.board.placeShip(two, 4, 5, "horizontal");
+game.cpuPlayer.board.placeShip(three, 8, 4, "horizontal");
+game.cpuPlayer.board.placeShip(four, 4, 0, "vertical");
+game.cpuPlayer.board.placeShip(five, 5, 4, "horizontal");
+
+renderBoards(game);
+
+console.log(game.humanPlayer.board.board);
+console.log(game.cpuPlayer.board.board);
