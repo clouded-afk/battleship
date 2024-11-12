10bc36f036a5c1eb1b7d2f9c6a622677b0c8b7ff
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-12 14:53:15 -0500

    fix: edit renderBoards to create the human and cpu gameboards seperately

--- src/modules/domController.js
+++ src/modules/domController.js
@@ -1,19 +1,32 @@
-function renderBoard(game) {
+function renderBoards(game) {
   const humanContainer = document.querySelector(".player-one-container");
   const cpuContainer = document.querySelector(".player-two-container");
 
-  const board = game.humanPlayer.board.board;
+  const humanBoard = game.humanPlayer.board.board;
 
-  board.forEach((row) => {
+  humanBoard.forEach((row) => {
     row.forEach((cell) => {
       const gridCellHuman = document.createElement("div");
-      const gridCellCpu = document.createElement("div");
       gridCellHuman.classList.add("cell");
-      gridCellCpu.classList.add("cell");
+      if (cell !== "hit" && cell !== "miss" && cell !== null) {
+        gridCellHuman.classList.add("ship");
+      }
       humanContainer.appendChild(gridCellHuman);
+    });
+  });
+
+  const cpuBoard = game.cpuPlayer.board.board;
+
+  cpuBoard.forEach((row) => {
+    row.forEach((cell) => {
+      const gridCellCpu = document.createElement("div");
+      gridCellCpu.classList.add("cell");
+      if (cell !== "hit" && cell !== "miss" && cell !== null) {
+        gridCellCpu.classList.add("ship");
+      }
       cpuContainer.appendChild(gridCellCpu);
     });
   });
 }
 
-export { renderBoard };
+export { renderBoards };
