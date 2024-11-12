f0f5b76046ef82f3282135753d2ec5161ff7350d
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-11 19:15:02 -0500

    test: create game and players to ensure renderBoard displays gameboards properly

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./style.css";
+import Game from "./modules/gameController";
+import { renderBoard } from "./modules/domController";
+
+const game = new Game();
+game.createPlayers();
+renderBoard(game);
