7f1225674951ff5b6a54ad3e7a01ca98aa1c39e4
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-13 17:42:50 -0500

    fix: fixed function typos

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -39,45 +39,45 @@ describe("Gameboard", () => {
 
   test("Invalid attack location", () => {
     expect(() => {
-      testBoard.recieveAttack(-1, 8);
+      testBoard.receiveAttack(-1, 8);
     }).toThrow("Invalid Coordinates to Attack!");
   });
 
   test("Coordinates Already Attacked, (hit)", () => {
     testBoard.placeShip(ship, 0, 0, "vertical");
-    testBoard.recieveAttack(4, 0);
+    testBoard.receiveAttack(4, 0);
 
     expect(() => {
-      testBoard.recieveAttack(4, 0);
+      testBoard.receiveAttack(4, 0);
     }).toThrow("Coordinates have already been attacked!");
   });
 
   test("Coordinates Already Attacked, (miss)", () => {
-    testBoard.recieveAttack(7, 5);
+    testBoard.receiveAttack(7, 5);
 
     expect(() => {
-      testBoard.recieveAttack(7, 5);
+      testBoard.receiveAttack(7, 5);
     }).toThrow("Coordinates have already been attacked!");
   });
 
   test("Attack Hits Ship", () => {
     testBoard.placeShip(ship, 0, 0, "vertical");
-    testBoard.recieveAttack(2, 0);
+    testBoard.receiveAttack(2, 0);
 
     expect(testBoard.board[2][0]).toBe("hit");
   });
 
   test("Attack Misses Ship", () => {
     testBoard.placeShip(ship, 0, 0, "vertical");
-    testBoard.recieveAttack(4, 7);
+    testBoard.receiveAttack(4, 7);
 
     expect(testBoard.board[4][7]).toBe("miss");
   });
 
   test("Hit increases Ship hits property", () => {
     testBoard.placeShip(ship, 0, 0, "vertical");
-    testBoard.recieveAttack(2, 0);
-    testBoard.recieveAttack(3, 0);
+    testBoard.receiveAttack(2, 0);
+    testBoard.receiveAttack(3, 0);
 
     expect(testBoard.board[0][0].hits).toBe(2);
   });
