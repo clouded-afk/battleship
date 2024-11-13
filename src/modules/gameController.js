5d43f07d85a7b29d616912438236b1e7c136788c
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-13 17:08:51 -0500

    feat: add function to simulate computer attacking the user

--- src/modules/gameController.js
+++ src/modules/gameController.js
@@ -2,6 +2,11 @@ import Gameboard from "../classes/gameboard";
 import Ship from "../classes/ship";
 import Player from "../classes/player";
 
+import {
+  cpuBoardEventHandler,
+  updatePlayerBoardDisplay,
+} from "./domController";
+
 export default class Game {
   constructor() {
     this.humanPlayer = null;
@@ -23,5 +28,20 @@ export default class Game {
   switchTurn() {
     this.currentTurn =
       this.currentTurn === this.humanPlayer ? this.cpuPlayer : this.humanPlayer;
+  }
+
+  sendCPUAttack() {
+    const humanBoard = this.humanPlayer.board;
+    let xCoordinate, yCoordinate;
+
+    do {
+      xCoordinate = Math.floor(Math.random() * 10);
+      yCoordinate = Math.floor(Math.random() * 10);
+    } while (
+      humanBoard.board[xCoordinate][yCoordinate] === "miss" ||
+      humanBoard.board[xCoordinate][yCoordinate] === "hit"
+    );
+
+    updatePlayerBoardDisplay(humanBoard, xCoordinate, yCoordinate);
   }
 }
