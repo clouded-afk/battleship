5ebfb33f75e5876f8e9a02f3e95097506a50da0b
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-20 16:10:45 -0500

    test: test to button to randomize ship placement

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import "./style.css";
 import Game from "./modules/gameController";
 import Ship from "./classes/ship";
-import { renderBoards, cpuBoardEventHandler } from "./modules/domController";
+import {
+  renderPlayerBoard,
+  recpuBoardEventHandler,
+  renderCPUBoard,
+  enableRandomizeButton,
+} from "./modules/domController";
 
 const game = new Game();
+
 game.startGame();
 
 game.randomizeShipPlacement(game.cpuPlayer.board);
 game.randomizeShipPlacement(game.humanPlayer.board);
+enableRandomizeButton(game);
 
-renderBoards(game);
-cpuBoardEventHandler(game);
+renderPlayerBoard(game);
+renderCPUBoard(game);
+console.log(game.humanPlayer.board.board);
