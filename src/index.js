c487ced772a9659bb99894b91c4370aaa40640d7
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-19 19:31:49 -0500

    test: test to ensure randomizeShip placement works correctly after the refactor

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import "./style.css";
 import Game from "./modules/gameController";
 import Ship from "./classes/ship";
-import { renderBoards } from "./modules/domController";
+import { renderBoards, cpuBoardEventHandler } from "./modules/domController";
 
 const game = new Game();
 game.startGame();
 
-const one = new Ship(5);
-const two = new Ship(4);
-const three = new Ship(3);
-const four = new Ship(3);
-const five = new Ship(2);
-
-game.humanPlayer.board.placeShip(one, 0, 0, "horizontal");
-game.humanPlayer.board.placeShip(two, 4, 5, "horizontal");
-game.humanPlayer.board.placeShip(three, 8, 4, "horizontal");
-game.humanPlayer.board.placeShip(four, 1, 0, "vertical");
-game.humanPlayer.board.placeShip(five, 5, 4, "vertical");
-
-game.randomizeCPUShipPlacement();
-console.log(game.countCPUShips());
+game.randomizeShipPlacement(game.cpuPlayer.board);
+game.randomizeShipPlacement(game.humanPlayer.board);
 
 renderBoards(game);
+cpuBoardEventHandler(game);
