c1ab45eb1310fcde928809f641d6a57e6b100b4f
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-27 15:13:12 -0500

    fix: remove obsolete imports and code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
 import "./style.css";
-import Game from "./modules/gameController";
-import Ship from "./classes/ship";
-import {
-  renderPlayerBoard,
-  cpuBoardEventHandler,
-  renderCPUBoard,
-  enableRandomizeButton,
-  enableStartButton,
-} from "./modules/domController";
+import { gameSetup } from "./modules/domController";
 
-const game = new Game();
-
-game.startGame();
-
-game.randomizeShipPlacement(game.cpuPlayer.board);
-game.randomizeShipPlacement(game.humanPlayer.board);
-enableRandomizeButton(game);
-enableStartButton(game);
-
-renderPlayerBoard(game);
+gameSetup();
